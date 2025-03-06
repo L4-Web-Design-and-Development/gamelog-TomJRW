fd6b17d4e2249347962e0a8ccec23dc2352e55d5
TomJRW <1740305@hc.ac.je>
2025-03-06 12:07:59 +0000

    index games front end

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -7,10 +10,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  const prisma = new PrismaClient()
+
+  const games = await prisma.game.findMany()
+
+  return json({ games })
+}
+
 export default function Index() {
+  const { games } = useLoaderData<typeof loader>()
+
+  console.log({ games })
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello, GameLogger!</h1>
+      <h1 className="text-4xl font-bold">Hello, world!</h1>
     </div>
   );
 }
